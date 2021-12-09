9ad5c5475dc863bd98a186414fd116ad039543d5
Krishna <krishnateja.kapu@outlook.com>
2021-12-09 17:19:48 +0530

    commit variables

---
+++ src/java_basics/Variables.java
@@ -0,0 +1,36 @@
+package java_basics;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 4;
+		
+		System.out.println(x+2);
+		System.out.println(x=='x');
+		
+		String s = "string";
+		System.out.println(s);
+		
+		int var1 = 999999999;
+		long var2 = 999999999999999999L;
+		short var3 = 9999;
+		byte var4 = 99;
+		double var5 = 999999999.99999;
+		boolean var6 = true;
+		char var7 = 'C';
+		
+		System.out.println(var1);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		System.out.println(var5);
+		System.out.println(var6);
+		System.out.println(var7);
+		
+		
+
+	}
+
+}
